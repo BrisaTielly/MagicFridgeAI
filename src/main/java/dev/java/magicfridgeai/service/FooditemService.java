44d5265112065ed7e7033a9eecab2ca1f1b6dbb6
Brisa Silva <brisatielly12@gmail.com>
2025-03-01 16:36:08 -0300

    feat: Adiciona os métodos principais da camada de service

--- src/main/java/dev/java/magicfridgeai/service/FooditemService.java
+++ src/main/java/dev/java/magicfridgeai/service/FooditemService.java
@@ -1,4 +1,43 @@
 package dev.java.magicfridgeai.service;
 
+import dev.java.magicfridgeai.entity.Fooditem;
+import dev.java.magicfridgeai.repository.FooditemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FooditemService {
+    private final FooditemRepository fooditemRepository;
+
+    public FooditemService(FooditemRepository fooditemRepository) {
+        this.fooditemRepository = fooditemRepository;
+    }
+
+    public Fooditem save(Fooditem fooditem){
+        return fooditemRepository.save(fooditem);
+    }
+
+    public Optional<Fooditem> findById(Long id){
+        return fooditemRepository.findById(id);
+    }
+
+    public List<Fooditem> findAll(Long id){
+        return fooditemRepository.findAll();
+    }
+
+    public Fooditem update(Long id, Fooditem fooditem){
+        Optional<Fooditem> oldItem = fooditemRepository.findById(id);
+        if (oldItem.isPresent()){
+            fooditem.setId(id);
+            return fooditemRepository.save(fooditem);
+        }
+        return null;
+    }
+
+    public void delete(Long id){
+        fooditemRepository.deleteById(id);
+    }
+
 }
